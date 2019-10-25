5914091613ec285312ceeba8e0424f149e5a8742
olliecomet <leyechi@gmail.com>
2019-10-24 22:30:36 -0700

    returns object with correct formatted timestamp as key and price as value

--- revenue/__test__/revenue.test.js
+++ revenue/__test__/revenue.test.js
@@ -2,8 +2,18 @@ const data = require('../transactions');
 const revenue = require('../revenue');
 
 describe('Revenue function', () => {
-  it('returns an object', () => {
+  it('returns an object with properly formated date as key', () => {
+    const transaction = [{
+      price: 1,
+      timestamp: 1563459038296
+    }];
 
+    const result = revenue(transaction);
+
+    expect(result).toEqual({ 'Thu Jul 18 2019': 1 });
+  });
+
+  it.skip('returns an object with formated keys and correct prices', () => {
     const result = revenue(data);
     expect(result).toBe();
   });
--- revenue/revenue.js
+++ revenue/revenue.js
@@ -1,5 +1,13 @@
 function revenue(transactions) {
-  return transactions;
+  const dailyRevenue = {};
+  let time = 0;
+
+  for(let i = 0; i < transactions.length; i++) {
+    time = new Date(transactions[i].timestamp).toString().substring(0, 15);
+    dailyRevenue[time] = transactions[i].price;
+  }
+
+  return dailyRevenue;
 }
 
 module.exports = revenue;
