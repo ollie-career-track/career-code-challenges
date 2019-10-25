6ca80254e9c261f6bf3b875faacfb9272e899ea0
olliecomet <leyechi@gmail.com>
2019-10-24 22:58:45 -0700

    trying to fix price error

--- revenue/revenue.js
+++ revenue/revenue.js
@@ -1,10 +1,12 @@
 function revenue(transactions) {
   const dailyRevenue = {};
   let time = 0;
+  let price = 0;
 
   for(let i = 0; i < transactions.length; i++) {
+    price = transactions[i].price;
     time = new Date(transactions[i].timestamp).toString().substring(0, 15);
-    dailyRevenue[time] = transactions[i].price;
+    dailyRevenue[time] = price;
   }
 
   return dailyRevenue;
